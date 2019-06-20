dd120eb72ff5f4b2f8bbf03bf480340bfbca8592
DING Liang <liangding.liam@gmail.com>
2019-06-20 19:49:55 +1000

    update pt-v2

---
+++ transformers/transformer-pt-v2/dataset.py
@@ -0,0 +1,5 @@
+# -*-coding:utf-8-*-
+# Author: alphadl
+# Email: liangding.liam@gmail.com
+# dataset.py 2019-06-20 19:48
+
---
+++ transformers/transformer-pt-v2/transformer/Constant.py
@@ -0,0 +1,14 @@
+# -*-coding:utf-8-*-
+# Author: alphadl
+# Email: liangding.liam@gmail.com
+# Constant.py 2019-06-20 16:24
+
+PAD = 0
+UNK = 1
+BOS = 2
+EOS = 3
+
+PAD_WORD = '<blank>'
+UNK_WORD = '<unk>'
+BOS_WORD = '<s>'
+EOS_WORD = '</s>'
