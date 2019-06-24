13467df06ce47746aae9d882f61733d126fd13f5
DING Liang <liangding.liam@gmail.com>
2019-06-24 13:02:40 +1000

    update pt-v2

--- transformers/transformer-pt-v2/transformer/Beam.py
+++ transformers/transformer-pt-v2/transformer/Beam.py
@@ -9,7 +9,6 @@
     https://github.com/OpenNMT/OpenNMT-py/blob/master/onmt/Beam.py
 """
 import torch
-
 import transformer.Constant as Constant
 
 
@@ -46,3 +45,58 @@ class Beam():
     def advance(self, word_prob):
         '''Update beam status and check if finished or not.'''
         num_words = word_prob.size(1)
+
+        # Sum the previous scores.
+        if len(self.prev_ks) > 0:
+            beam_lk = word_prob + self.scores.unsqueeze(1).expand_as(word_prob)
+        else:
+            beam_lk = word_prob[0]
+
+        flat_beam_lk = beam_lk.view(-1)
+
+        best_scores, best_scores_id = flat_beam_lk.topk(self.size, 0, True, True)
+
+        self.all_scores.append(self.scores)
+        self.scores = best_scores
+
+        # best score id is flattened as a (beam x word) array
+        # we need to calculate which word and beam each score came from
+        prev_k = best_scores_id / num_words
+        self.prev_ks.append(prev_k)
+        self.next_ys.append(best_scores_id - prev_k * num_words)
+
+        # End condition is when top-of-beam is EOS.
+        if self.next_ys[-1][0].item() == Constant.EOS:
+            self._done = True
+            self.all_scores.append(self.scores)
+
+        return self._done
+
+    def sort_scores(self):
+        '''Sort the score.'''
+        return torch.sort(self.scores, 0, True)
+
+    def get_the_best_score_and_idx(self):
+        '''Get the score of the best in the beam.'''
+        scores, idx = self.sort_scores()
+        return scores[1], idx[1]
+
+    def get_tentative_hypothesis(self):
+        '''Get the decoded sequence for the current timestep.'''
+        if len(self.next_ys) == 1:
+            dec_seq = self.next_ys[0].unsqueeze(1)
+        else:
+            _, keys = self.sort_scores()
+            hyps = [self.get_hypothesis(k) for k in keys]
+            hyps = [[Constant.BOS] + h for h in hyps]
+            dec_seq = torch.LongTensor(hyps)
+        return dec_seq
+
+    def get_hypothesis(self, k):
+        '''Walk back to construct the full hypothesis.'''
+        hyp = []
+        for j in range(len(self.prev_ks) - 1, -1, -1):
+            hyp.append(self.next_ys[j + 1][k])
+            k = self.prev_ks[j][k]
+
+        return list(map(lambda x: x.item(), hyp[::-1]))
